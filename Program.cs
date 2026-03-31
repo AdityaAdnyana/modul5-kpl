510b1283c64dc3f5a9ce071ae91e173d855c27ff
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-03-31 11:37:36 +0800

    feat: menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using modul5_103082400005;
+
+public class Program()
+{
+    public static void Main()
+    {
+        SimpleDataBase<int> simpleDataBase = new();
+        simpleDataBase.AddNewData(12);
+        simpleDataBase.AddNewData(34);
+        simpleDataBase.AddNewData(56);
+
+        simpleDataBase.PrintAllData();
+    }
+}
