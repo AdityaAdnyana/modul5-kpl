4978ba85600cccc13922ed6471cbc507aa1e29cc
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-03-31 10:09:30 +0800

    feat: menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ Penjumlahan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul5_103082400005
+{
+    internal class Penjumlahan<T>
+    {
+        public T JumlahTigaAngka(T a, T b, T c)
+        {
+            dynamic da = a;
+            dynamic db = b;
+            dynamic dc = c;
+
+            var hasilJumlah = da + db + dc;
+
+            return hasilJumlah;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul5_103082400005;
+
+public class Program
+{
+    private static void Main()
+    {
+        Penjumlahan<double> penjumlahan = new();
+        object myDouble = penjumlahan.JumlahTigaAngka(1.0,3.0,8.2);
+
+        Console.WriteLine("Hasil penjumlahan double: " + myDouble);
+    }
+}
